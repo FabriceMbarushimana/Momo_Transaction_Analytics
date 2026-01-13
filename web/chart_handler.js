aedbfbaf5efc25404b2c0b5141dfc36a843211be
Waliyat Badmus <w.badmus@alustudent.com>
2026-01-13 15:24:16 +0000

    created basic structures for the web

---
+++ web/chart_handler.js
@@ -0,0 +1,10 @@
+// This file will later handle fetching data and rendering charts
+
+console.log("Chart handler loaded");
+
+// Placeholder function
+function loadDashboard() {
+    console.log("Dashboard will load data here");
+}
+
+loadDashboard();
